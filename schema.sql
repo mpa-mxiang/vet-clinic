af778c9a4fe1af1eebbd11270c3874672918ecea
Jiachun Xiang <max997123@gmail.com>
2023-07-09 17:38:25 -0400

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of the entire database. */
+
+CREATE TABLE animals (
+    id int
+    name varchar(100)
+    date_of_birth date
+    escape_attempts int
+    neutered bool
+    weight_kg float(255)
+);
