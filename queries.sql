36cddbc7bfb57eb17e2e521a44e1424795bf5215
Jiachun Xiang <max997123@gmail.com>
2023-07-09 23:39:29 -0400

    add queries

---
+++ queries.sql
@@ -0,0 +1,22 @@
+/*Queries that provide answers to the questions from all projects.*/
+
+SELECT * FROM animals
+WHERE name LIKE '%mon';
+
+SELECT name FROM animals
+WHERE date_of_birth >= '2016/01/01' and date_of_birth <= '2019/12/31';
+
+SELECT date_of_birth FROM animals
+WHERE name = 'Agumon' and name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals
+WHERE weight_kg > 10.5;
+
+SELECT * FROM animals
+WHERE neutered = 1;
+
+SELECT * FROM animals
+WHERE name != 'Gabumon';
+
+SELECT * FROM animals
+WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
