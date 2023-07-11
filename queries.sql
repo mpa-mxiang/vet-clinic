7e30ee91247fd78094439213de5da7c09574d176
Jiachun Xiang <max997123@gmail.com>
2023-07-11 18:45:44 -0400

    add more queries to answer questions

--- queries.sql
+++ queries.sql
@@ -61,3 +61,43 @@ SAVEPOINT my_savepoint;
 -- Update all animals' weight to be their weight multiplied by -1.
 UPDATE animals SET weight_kg = weight_kg * -1;
 COMMIT;
+
+
+------------------------------------------------------------
+-- animal count
+SELECT DISTINCT COUNT(name) FROM animals;
+-- How many animals have never tried to escape?
+SELECT DISTINCT COUNT(name)
+FROM animals
+WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg)
+FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT SUM(escape_attempts)
+FROM animals
+WHERE neutered=TRUE;
+
+SELECT SUM(escape_attempts)
+FROM animals
+WHERE neutered=FALSE;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT MIN(weight_kg), MAX(weight_kg)
+FROM animals
+WHERE neutered=TRUE;
+
+SELECT MIN(weight_kg), MAX(weight_kg)
+FROM animals
+WHERE neutered=FALSE;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG(escape_attempts)
+FROM animals
+WHERE neutered=TRUE
+AND date_of_birth >= '1990/01/01' and date_of_birth <= '2000/12/31';
+
+SELECT AVG(escape_attempts)
+FROM animals
+WHERE neutered=FALSE
+AND date_of_birth >= '1990/01/01' and date_of_birth <= '2000/12/31';
