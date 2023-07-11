ab2c04e79a5f5ab71af7f295e8d286274ad60959
Jiachun Xiang <max997123@gmail.com>
2023-07-11 18:18:43 -0400

    copy new contents from update.sql

--- queries.sql
+++ queries.sql
@@ -24,3 +24,40 @@ WHERE name != 'Gabumon';
 
 SELECT * FROM animals
 WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
+
+
+---------------
+UPDATE animals SET species = 'unspecified' WHERE id = 3;
+
+-- Then roll back the change and verify that the species columns went back to the state before the transaction:
+
+UPDATE animals SET species = null WHERE id = 3;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set
+
+UPDATE animals SET species = 'pokemon' WHERE species IS null;
+
+-- Commit the transaction.
+
+COMMIT;
+
+---------------------------------------------
+-- Begin a new transaction.
+BEGIN;
+-- Delete all records in the animals table.
+DELETE FROM animals;
+-- Rollback.
+ROLLBACK;
+
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+-- Create a savepoint for the transaction.
+BEGIN;
+SAVEPOINT my_savepoint;
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+COMMIT;
