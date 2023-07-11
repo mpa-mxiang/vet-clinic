f23af15fa1e97b3359f11d85d53ac612d2e50028
Jiachun Xiang <max997123@gmail.com>
2023-07-11 11:37:29 -0400

    change WHERE condition for 4th query

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT name FROM animals
 WHERE date_of_birth >= '2016/01/01' and date_of_birth <= '2019/12/31';
 
 SELECT date_of_birth FROM animals
-WHERE name = 'Agumon' and name = 'Pikachu';
+WHERE name = 'Agumon' OR name = 'Pikachu';
 
 SELECT name, escape_attempts FROM animals
 WHERE weight_kg > 10.5;
