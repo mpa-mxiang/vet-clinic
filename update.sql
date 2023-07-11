82d08a3537ac84756163ef77bbef0fb98eece0a1
Jiachun Xiang <max997123@gmail.com>
2023-07-11 18:07:42 -0400

    add a new transaction

--- update.sql
+++ update.sql
@@ -7,3 +7,24 @@ UPDATE animals SET species = 'unspecified' WHERE id = 3;
 UPDATE animals SET species = null WHERE id = 3;
 
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set
+
+UPDATE animals SET species = 'pokemon' WHERE species IS null;
+
+-- Commit the transaction.
+
+COMMIT;
+
+---------------------------------------------
+-- Begin a new transaction.
+BEGIN;
+-- Delete all records in the animals table.
+DELETE FROM animals;
+-- Rollback.
+ROLLBACK;
+
+-- Delete all animals born after Jan 1st, 2022.
+
