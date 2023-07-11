e6473549db81069f9cfa2acec8e9377ce4dabd61
Jiachun Xiang <max997123@gmail.com>
2023-07-11 17:40:49 -0400

    insert more data

--- data.sql
+++ data.sql
@@ -11,3 +11,35 @@ VALUES ('Pikachu', TO_DATE('07/01/2021', 'DD/MM/YYYY'), 1, 0, 15.04);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', TO_DATE('12/05/2017', 'DD/MM/YYYY'), 5, 1, 11);
+
+/*
+Insert the following data:
+Animal: His name is Charmander. He was born on Feb 8th, 2020, and currently weighs -11kg. He is not neutered and he has never tried to escape.
+Animal: Her name is Plantmon. She was born on Nov 15th, 2021, and currently weighs -5.7kg. She is neutered and she has tried to escape 2 times.
+Animal: His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to escape 3 times.
+Animal: His name is Angemon. He was born on Jun 12th, 2005, and currently weighs -45kg. He is neutered and he has tried to escape once.
+Animal: His name is Boarmon. He was born on Jun 7th, 2005, and currently weighs 20.4kg. He is neutered and he has tried to escape 7 times.
+Animal: Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times.
+Animal: His name is Ditto. He was born on May 14th, 2022, and currently weighs 22kg. He is neutered and he has tried to escape 4 times.
+*/
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', TO_DATE('08/02/2020', 'DD/MM/YYYY'), 0, FALSE, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Plantmon', TO_DATE('15/11/2021', 'DD/MM/YYYY'), 2, TRUE, 5.7);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Squirtle', TO_DATE('02/04/1993', 'DD/MM/YYYY'), 3, FALSE, 12.13);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Angemon', TO_DATE('12/06/2005', 'DD/MM/YYYY'), 1, TRUE, 45);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Boarmon', TO_DATE('07/06/2005', 'DD/MM/YYYY'), 7, TRUE, 20.4);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Blossom', TO_DATE('13/10/1998', 'DD/MM/YYYY'), 3, TRUE, 17);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Ditto', TO_DATE('14/05/2022', 'DD/MM/YYYY'), 4, TRUE, 22);
