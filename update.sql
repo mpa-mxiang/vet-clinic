9dd44164e631cecc19a8355ba880212fd9a2ba92
Jiachun Xiang <max997123@gmail.com>
2023-07-17 15:22:16 -0400

    insert three required queries

--- update.sql
+++ update.sql
@@ -1,36 +1,10 @@
--- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made.
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
-UPDATE animals SET species = 'unspecified' WHERE id = 3;
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
--- Then roll back the change and verify that the species columns went back to the state before the transaction:
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
-UPDATE animals SET species = null WHERE id = 3;
-
--- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
-
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-
--- Update the animals table by setting the species column to pokemon for all animals that don't have species already set
-
-UPDATE animals SET species = 'pokemon' WHERE species IS null;
-
--- Commit the transaction.
-
-COMMIT;
-
----------------------------------------------
--- Begin a new transaction.
-BEGIN;
--- Delete all records in the animals table.
-DELETE FROM animals;
--- Rollback.
-ROLLBACK;
-
--- Delete all animals born after Jan 1st, 2022.
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
--- Create a savepoint for the transaction.
-BEGIN;
-SAVEPOINT my_savepoint;
--- Update all animals' weight to be their weight multiplied by -1.
-UPDATE animals SET weight_kg = weight_kg * -1;
-COMMIT;
+--------------------------------------------------------------------------------------------------------------------------------------------
