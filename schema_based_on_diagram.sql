c653c98e22ed07520da5032f640c6349d7f4109a
Jiachun Xiang <max997123@gmail.com>
2023-07-18 18:54:24 -0400

    Revert "treatment and medical tables added."

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,18 +1,1 @@
-CREATE TABLE treatments (
-    id INT,
-    type VARCHAR(30),
-    name VARCHAR(30),
-    PRIMARY KEY(id),
-);
 
---  FOREIGN KEY(vet_id) REFERENCES vets(id),
---     FOREIGN KEY(species_id) REFERENCES species(id)
-
-CREATE TABLE medical_histories (
-    id INT,
-    admitted_at VARCHAR(30),
-    patient_id INT,
-    status VARCHAR(30),
-    PRIMARY KEY(id),
-    FOREIGN KEY(patient_id) REFERENCES patients(id)
-);
