5dc7ea537e5fda2dddbd058b5e186c5e1ae7895a
Jiachun Xiang <max997123@gmail.com>
2023-07-18 18:46:37 -0400

    CREATE patients and invoices tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,15 @@
+CREATE TABLE patients(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
+);
 
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amountsql DECIMAL,
+  generated_at TIMESTAMP,
+  generated_at TIMESTAMP,
+  medical_history_id INT
+  PRIMARY KEY(id)
+);
